232a1fa74f4e4c4407495490184227b6679fa187
Chris Filkins <filchyboy@gmail.com>
2020-11-06 18:11:30 -0800

    Added Markov chains

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,13 @@
 def no_dups(s):
-    # Your code here
+    duplicates = {}
+    words = s.split(" ")
+    new_string = []
+    for word in words:
+        if word not in duplicates:
+            duplicates[word] = 1
+            new_string.append(word)
+    new_string = ' '.join(new_string)
+    return new_string
 
 
 
@@ -8,4 +16,4 @@ if __name__ == "__main__":
     print(no_dups("hello"))
     print(no_dups("hello hello"))
     print(no_dups("cats dogs fish cats dogs"))
-    print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+    print(no_dups("spam spam spam eggs and spam and sausage but no spam spam and spam"))
